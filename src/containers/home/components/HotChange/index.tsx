df2a7e4e7fba09840a0a5f11fd596b91b824a1a9
jack-yong <1807413446@qq.com>
2022-09-21 10:39:29 +0800

    ✨新增(typescript): 博客项目重构init

--- src/containers/home/components/HotChange/index.tsx
+++ src/containers/home/components/HotChange/index.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
-    const [count, setCounts] = useState('')
+    const [count, setCounts] = useState('');
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const onChange = (e: any) => {
-        setCounts(e.target.value)
-    }
+        setCounts(e.target.value);
+    };
     return (
         <>
             <h2>webpack5+react+ts</h2>
             <p>受控组件</p>
-            <input type="text" value={count} onChange={onChange} />
+            <input type='text' value={count} onChange={onChange} />
             <br />
-            <p>非受控组件ssssss</p>
-            <input type="text" />
+            <p>非受控组件</p>
+            <input type='text' />
         </>
-    )
+    );
 }
-export default App
+export default App;
