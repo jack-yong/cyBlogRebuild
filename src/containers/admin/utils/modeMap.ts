c12f6ae24b8fee6032b4d25643d3e73a512ee05d
jack-yong <1807413446@qq.com>
2022-11-10 18:19:52 +0800

    ♻️重构(typescript): 调整dark模式下的相关样式
    调整dark模式下有关样式并且设置相应的全局属性

--- src/containers/admin/utils/modeMap.ts
+++ src/containers/admin/utils/modeMap.ts
@@ -1,6 +1,7 @@
 export const modeMap = {
     '--themeColor': ['rgb(255, 255, 255)', 'rgb(0 21 41)'],
     '--themeColor1': ['rgb(240, 242, 245)', 'rgb(0,12,23)'],
+    '--themeColor2': ['rgb(240, 242, 245)', 'rgb(0,33,64)'],
     '--textColor': ['rgb(0, 0, 0)', 'rgb(255 ,255 ,255 , 0.65)'],
     '--switchOff': ['rgb(77, 77, 77)', 'rgb(179, 198, 207)'],
     '--tip': ['rgb(116, 116, 116)', 'rgb(180, 180, 180)'],
