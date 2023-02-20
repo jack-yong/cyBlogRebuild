cc98eaec5b7b7a6f16889e3a2102d003167184e2
caiyong3 <caiyong3@iresearch.com.cn>
2023-02-20 20:08:31 +0800

    ✨新增(typescript): 增加面包屑功能

--- src/containers/admin/layouts/Content/index.tsx
+++ src/containers/admin/layouts/Content/index.tsx
@@ -1,20 +1,47 @@
 import React, { Suspense } from 'react';
-import { Layout, Spin } from 'antd';
+import { Breadcrumb, Layout, Space, Spin } from 'antd';
 import ErrorBoundary from '../../components/ErrorBoundary';
-import { Outlet } from 'react-router-dom';
+import { Link, matchRoutes, Outlet, useLocation } from 'react-router-dom';
 import s from './index.mod.scss';
+import routes from '../../routes';
+import { HomeOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { configStore } from '../../store';
+import { useSnapshot } from 'valtio';
 
 const { Content } = Layout;
 
 const index: React.FC = () => {
+    const location = useLocation();
+    const matches = matchRoutes(routes, location);
+
+    /* renders */
+    const renderBreadcrumb = () => {
+        return (matches ?? []).map(i => {
+            return (
+                <Breadcrumb.Item key={i.pathname}>
+                    <Link to={i.route.path || '/'} key={i.pathname} className={s.bItem}>
+                        {i.route?.handle?.title ?? <HomeOutlined />}
+                    </Link>
+                </Breadcrumb.Item>
+            );
+        });
+    };
+
     return (
-        <Content className={s.content}>
-            <ErrorBoundary>
-                <Suspense fallback={<Spin style={{ margin: 'auto' }} size='large' />}>
-                    <Outlet />
-                </Suspense>
-            </ErrorBoundary>
-        </Content>
+        <>
+            <Space size='large' className={s.breadcrumb}>
+                <Breadcrumb separator={<span className={s.bItem}>/</span>}>
+                    {renderBreadcrumb()}
+                </Breadcrumb>
+            </Space>
+            <Content className={s.content}>
+                <ErrorBoundary>
+                    <Suspense fallback={<Spin style={{ margin: 'auto' }} size='large' />}>
+                        <Outlet />
+                    </Suspense>
+                </ErrorBoundary>
+            </Content>
+        </>
     );
 };
 
