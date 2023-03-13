dc7e616ea91288a4d23f2b26b3f8d1c92bdaa273
cyong <1807413446@qq.com>
2023-03-13 20:39:48 +0800

    ✨新增(javascript): 增加生产开发环境publicpath配置

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -22,7 +22,7 @@ module.exports = merge(baseConfig, {
         chunkFilename: 'static/js/[name].[chunkhash:8].js', // js的名称
         path: path.resolve(ROOT_PATH, './dist'), // 打包的出口文件夹路径
         clean: true, // webpack4需要配置clean-webpack-plugin删除dist文件，webpack5内置了。
-        publicPath: './' // 打包后文件的公共前缀路径
+        publicPath: process.env.PUBLIC_PATH // 打包后文件的公共前缀路径
     },
     plugins: [
         // 复制文件插件
